dcf82a969d02a8260e11b2e93203ef3ed7edd1a9
Pranay Kadu <pranayp.kadu@gmail.com>
2025-01-14 01:03:41 +0530

    Changes

---
+++ Core_Java_A_to_Z/Arrayp1.java
@@ -0,0 +1,14 @@
+class Arrayp1
+{
+    public static void main(String [] args){
+
+        //declare and initialize the array
+        int arr [] = {50, 60, 70, 80, 90};
+        // display all the 5 elements
+        for (int i=0; i<5; i++){
+            System.out.println(arr[i]);
+        }
+
+
+    }
+}
